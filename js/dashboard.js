6308a6fc765b151d0b64c11f7718e2836b753fbe
George Heeres <gheeres@gmail.com>
2022-12-15 18:23:10 -0600

    Initial commit

---
+++ js/dashboard.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict'
+  feather.replace({ 'aria-hidden': 'true' });
+
+  /**
+   * Generates a random number.
+   * @param {Number} max The maximum value to include.
+   * @returns The random number
+   */
+  function random(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+})();
